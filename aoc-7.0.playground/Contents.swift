e0e9c7f8b02ae3d06d3676fac46978003f60aec2
Jordan Griffin <jordan.griffin@asynchrony.com>
2017-12-11 21:23:05 -0600

    finished 7.0, skipping 7.1 for now because my brain hurts - JG

--- aoc-7.0.playground/Contents.swift
+++ aoc-7.0.playground/Contents.swift
@@ -1,14 +1,32 @@
 import Foundation
 
-struct Node {
+class Node {
     let name: String
     let weight: Int
-    let childNodes: [String]
+    let childNames: [String]
+    var childNodes = [Node]()
+    
+    init(name: String, weight: Int, childNames: [String]) {
+        self.name = name
+        self.weight = weight
+        self.childNames = childNames
+    }
 }
 
 extension Node : CustomStringConvertible {
+    
+    func stringify(withIndent indent: Int) -> String {
+        var space = String.init(repeating: " ", count: indent * 2)
+        var result = space + "Node(\(name), \(weight)) [\(childNodes.count > 0 ? "\n" : "")"
+        childNodes.forEach {
+            result += $0.stringify(withIndent: indent + 1) + "\n"
+        }
+        result += "\(childNodes.count > 0 ? space : "")]"
+        return result
+    }
+    
     var description: String {
-        return "Node(\(name), \(weight), \(childNodes))"
+        return stringify(withIndent: 0)
     }
 }
 
@@ -29,12 +47,51 @@ cntj (57)
 """
 
 let tower2 = tower.split(separator: "\n")
-    .map { $0.replacingOccurrences(of: " -> ", with: ">").split(separator: ">")
-        .map { $0.replacingOccurrences(of: " (", with: "(")
-            .split(separator: "(").map { $0.replacingOccurrences(of: ")", with: "") }
-        } }
-    .map { Node(name: $0.first![0], weight: Int($0.first![1])!, childNodes: $0.count == 2 ? $0[1] : []) }
+    .map {
+        $0.replacingOccurrences(of: " -> ", with: ">")
+            .replacingOccurrences(of: ", ", with: ",")
+            .split(separator: ">")
+            .map {
+                $0.replacingOccurrences(of: " (", with: "(")
+                    .split(separator: "(")
+                    .map { $0.replacingOccurrences(of: ")", with: "") }
+            }
+    }
+    .map {
+        (thing: [[String]]) -> Node in
+        let childNames = thing.count == 2 ? thing[1][0].split(separator: ",").map(String.init) : [String]()
+        return Node(name: thing.first![0], weight: Int(thing.first![1])!, childNames: childNames)
+    }
 
+let towerDict = tower2
+    .reduce([String : Node]()) {
+        var newDict = $0
+        newDict[$1.name] = $1
+        return newDict
+    }
 
-tower2.forEach { print($0) }
+for tower in tower2 {
+    tower.childNodes = tower.childNames.map { towerDict[$0]! }
+}
 
+var nameArray = [String]()
+tower2.forEach {
+    if $0.childNodes.count > 0 {
+        print($0)
+    }
+    
+    nameArray.append($0.name)
+    for childNode in $0.childNodes {
+        nameArray.append(childNode.name)
+    }
+}
+
+var root = ""
+nameArray = nameArray.sorted()
+for i in stride(from: 0, to: nameArray.count, by: 2) {
+    if nameArray[i] != nameArray[i + 1] {
+        root = String(nameArray[i])
+        break
+    }
+}
+print(root)
