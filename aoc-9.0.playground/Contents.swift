06d5f017dcab08e4a753ba2847258bda5e9bc083
dev1 <dev1@spr-md12.wwt.com>
2018-02-08 13:07:44 -0600

    aoc-9.0 - refactored code - JG MD

--- aoc-9.0.playground/Contents.swift
+++ aoc-9.0.playground/Contents.swift
@@ -10,35 +10,23 @@ let input7 = "{{<a>},{<a>},{<a>},{<a>}}"
 let input8 = "{{<!>},{<!>},{<!>},{<a>}}"
 
 func getGroupCount(_ input: String) -> Int {
-    return input.canceledRemoved().garbageRemoved().filter { (char : Character) -> Bool in char == "{" }.count
+    let canceledCharPattern = "!."
+    let garbagePattern = "<[^>]*>"
+    return input
+        .removePattern(matching: canceledCharPattern)
+        .removePattern(matching: garbagePattern)
+        .filter { (char : Character) -> Bool in char == "{" }
+        .count
 }
 
 extension String {
-    func garbageRemoved() -> String {
-        guard let regex = try? NSRegularExpression(pattern: "<[^>]*>") else { return self }
-        var result = NSMutableString(string: self)
-        regex.replaceMatches(in: result, range: NSMakeRange(0, result.length), withTemplate: "")
-        return String(result)
-    }
-    
-    func canceledRemoved() -> String {
-        guard let regex = try? NSRegularExpression(pattern: "!.") else { return self }
+    func removePattern(matching pattern: String) -> String {
+        guard let regex = try? NSRegularExpression(pattern: pattern) else { return self }
         var result = NSMutableString(string: self)
         regex.replaceMatches(in: result, range: NSMakeRange(0, result.length), withTemplate: "")
         return String(result)
     }
 }
-
-input8.canceledRemoved() == "{{<},{<},{<},{<a>}}"
-
-input1.garbageRemoved() == "{}"
-input2.garbageRemoved() == "{{{}}}"
-input3.garbageRemoved() == "{{},{}}"
-input4.garbageRemoved() == "{{{},{},{{}}}}"
-input5.garbageRemoved() == "{}"
-input6.garbageRemoved() == "{,,,}"
-input7.garbageRemoved() == "{{},{},{},{}}"
-input8.garbageRemoved() == "{{},{},{},{}}"
 
 getGroupCount(input1) == 1
 getGroupCount(input2) == 3
